c329509d5173e57184140abeefe6c2f0c28e1541
jnemo44 <64214120+jnemo44@users.noreply.github.com>
2020-12-30 16:08:57 -0600

    Added a comment. Will revisit this project after continuing to learn forms in React

--- src/components/Trade/TradeInfo/TradeInfo.js
+++ src/components/Trade/TradeInfo/TradeInfo.js
@@ -1,8 +1,11 @@
 //This file is to allow a new trade to be input
+//Ticker/#Contracts/SellorBuy/OpenPrice/OpenDate/Type
 import React from 'react';
 
 const tradeInfo = (props) => {
-
+    <div>
+        <input></input>
+    </div>
 }
 
 export default tradeInfo;
