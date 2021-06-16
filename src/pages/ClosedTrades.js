391797d49fe82e7790009b0d7b39bbd7e9dcafd2
Joe Niemiec <>
2021-06-15 21:14:44 -0500

    Added get fetch to closed trades tab. Needs work.

--- src/pages/ClosedTrades.js
+++ src/pages/ClosedTrades.js
@@ -1,7 +1,51 @@
-function ClosedTradesPage () {
+import { useState, useEffect } from 'react';
+
+function ClosedTradesPage() {
+    //const [displayModal, setDisplayModal] = useState(false);
+    //const [newTradeAdded, setNewTradeAdded] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [loadedTrades, setLoadedTrades] = useState([]);
+
+    useEffect(() => {
+        var one_day = 1000 * 60 * 60 * 24;
+        var currentDate = new Date();
+
+        //setIsLoading(true);
+        fetch("http://127.0.0.1:5000/close-orders", {
+            method: "GET",
+            headers: { "Content-type": "application/json" },
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const trades = [];
+                const convertedData = { ...data.close_list };
+                console.log("TEST", convertedData);
+                for (const key in convertedData) {
+                    console.log("Key", key);
+                    const trade = {
+                        id: key,
+                        ...convertedData[key],
+                    };
+                    trades.push(trade);
+                }
+                setLoadedTrades(trades);
+                //setIsLoading(false);
+                //setNewTradeAdded(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section>
+                <h1>Loading...</h1>
+            </section>
+        );
+    }
     return (
         <div>
-            Closed Trades Page
+            OUTPUT HERE
         </div>
     )
 }
