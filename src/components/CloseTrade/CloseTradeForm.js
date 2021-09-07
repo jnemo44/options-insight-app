0ef4e90ba07a087055e3e0199d0a053309e14903
Joe Niemiec <joe.niemiec@gmail.com>
2021-09-06 21:57:52 -0500

    Made the cancel and submit button styling global to reduce redundant code and also deleted some public files not needed

--- src/components/CloseTrade/CloseTradeForm.js
+++ src/components/CloseTrade/CloseTradeForm.js
@@ -120,19 +120,13 @@ function CloseTradeForm(props) {
         <Button
           type="submit"
           name="Submit"
-          className="mt-4 mr-4 w-full inline-flex justify-center rounded-md border border-transparent 
-          shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 
-          focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-0 
-          sm:w-auto sm:text-sm">
+          className="btn-submit">
         </Button>
         <Button
           type="button"
           onClick={props.onCancel}
           name="Cancel"
-          className="mt-4 w-full inline-flex justify-center rounded-md border border-gray-300 
-            shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500
-            focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 
-            sm:w-auto sm:text-sm">
+          className="btn-cancel">
         </Button>
       </div>
     </form>
