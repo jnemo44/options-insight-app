7f77b15e9182335ca03535b44c53703998c90457
Joe Niemiec <joe.niemiec@gmail.com>
2021-12-11 17:21:07 -0600

    Changed header button color

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -10,7 +10,7 @@ function MainNavigation() {
   return (
     <header className="bg-gray-800">
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-        <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+        <div className="w-full py-6 flex items-center justify-between border-b border-green-600 lg:border-none">
           <div className="flex items-center">
             <a href="#">
               <span className="sr-only">Workflow</span>
@@ -25,7 +25,7 @@ function MainNavigation() {
                 <Link
                   key={link.name}
                   to={link.href}
-                  className="text-base font-medium text-white hover:text-indigo-50"
+                  className="text-base font-medium text-white hover:text-green-50"
                 >
                   {link.name}
                 </Link>
@@ -35,13 +35,13 @@ function MainNavigation() {
           <div className="ml-10 space-x-4">
             <a
               href="#"
-              className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+              className="inline-block bg-green-600 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
             >
               Log Out
             </a>
             <a
               href="#"
-              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-green-900 hover:bg-green-50"
             >
               Sign up
             </a>
